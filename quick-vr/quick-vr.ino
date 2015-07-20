53a225f459601be7ef35f7b436fb98fb70f74ab0
Mmmmmmie <mmmmmmiemie@gmail.com>
2015-07-20 16:40:42 +0800

    quick-vr

---
+++ quick-vr/quick-vr.ino
@@ -0,0 +1,79 @@
+ int x=800;
+ int y=300;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);  
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(7,OUTPUT);
+  pinMode(8,OUTPUT);
+  pinMode(9,OUTPUT);
+ 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(2,HIGH); 
+ delay(analogRead(A0));
+ digitalWrite(2,LOW);
+  delay(analogRead(A0)/2);
+ digitalWrite(3,HIGH);
+   delay(analogRead(A0));
+   digitalWrite(3,LOW);
+    delay(analogRead(A0)/2);  
+  digitalWrite(4,HIGH);
+   delay(analogRead(A0)); 
+   digitalWrite(4,LOW);
+    delay(analogRead(A0)/2); 
+  digitalWrite(5,HIGH);
+   delay(analogRead(A0));
+   digitalWrite(5,LOW);
+    delay(analogRead(A0)/2);
+  digitalWrite(6,HIGH);
+   delay(analogRead(A0));
+   digitalWrite(6,LOW);
+    delay(analogRead(A0)/2);
+  digitalWrite(7,HIGH);
+   delay(analogRead(A0));
+   digitalWrite(7,LOW);
+    delay(analogRead(A0)/2);
+  digitalWrite(8,HIGH);
+   delay(analogRead(A0));
+   digitalWrite(8,LOW);
+    delay(analogRead(A0)/2);
+  digitalWrite(9,HIGH);
+   delay(analogRead(A0));
+   digitalWrite(9,LOW);
+   delay(analogRead(A0)/2);
+   digitalWrite(8,HIGH);
+   delay(analogRead(A0));
+   digitalWrite(8,LOW);
+    delay(analogRead(A0)/2);
+    digitalWrite(7,HIGH);
+   delay(analogRead(A0));
+   digitalWrite(7,LOW);
+    delay(analogRead(A0)/2);
+    digitalWrite(6,HIGH);
+   delay(analogRead(A0));
+   digitalWrite(6,LOW);
+    delay(analogRead(A0)/2);
+    digitalWrite(5,HIGH);
+   delay(analogRead(A0));
+   digitalWrite(5,LOW);
+    delay(analogRead(A0)/2);
+    digitalWrite(4,HIGH);
+   delay(analogRead(A0)); 
+   digitalWrite(4,LOW);
+    delay(analogRead(A0)/2); 
+    digitalWrite(3,HIGH);
+   delay(analogRead(A0));
+   digitalWrite(3,LOW);
+    delay(analogRead(A0)/2);  
+     digitalWrite(2,HIGH); 
+      delay(analogRead(A0));
+ digitalWrite(2,LOW);
+ 
+
+}
